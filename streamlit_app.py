cf5092c51da8963ec73c9ef7ac5e14d1ef8fa290
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2025-12-30 20:45:58 +0000

    Update 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Tarek Khorshed's App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.write("My first python APP.")
