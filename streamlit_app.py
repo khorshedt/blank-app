1a25801901304425ee55abf3cc56dd105ae25c6f
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2026-01-12 13:37:33 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,10 @@ df = pd.DataFrame({
 st.write(df)
 
 
-
+# Dynamic table
 df2 = np.random.randn(10, 20)
 st.dataframe(df2)
+st.dataframe(df2.style.highlight_max(axis=0))
+
+# static table
+st.table(df2)  
