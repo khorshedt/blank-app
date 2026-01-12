a01f2c4d6369cc1c7628d8fdbee66e5621715246
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2026-01-12 13:29:45 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 Tarek Khorshed's App 2025")
 #st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
@@ -7,3 +8,13 @@ st.title("🎈 Tarek Khorshed's App 2025")
 st.write("My first python APP 2025.")
 st.write("Now you need to code ya Aloshi")
 st.write("Test2")
+
+
+
+
+df = pd.DataFrame({
+  'column1': [1, 2, 3, 4],
+  'column2': [10, 20, 30, 40]
+})
+
+df
