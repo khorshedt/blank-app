1bc767b88a467b12eb7630e201f4fd93bcc633b0
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2026-01-12 19:12:23 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,18 @@ with st.form(key='my_form'):
 
 
 
+# Sidebar
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
 
 
 if st.checkbox('Show dataframe'):
