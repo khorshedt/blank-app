03b84daffc2da27a312b638902d06097d9acea2d
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2026-01-12 15:14:40 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,22 @@ st.write(st.session_state)
 
 
 
+
+def form_callback():
+    st.write(st.session_state.my_slider)
+    st.write(st.session_state.my_checkbox)
+    st.write(st.session_state.my_textbox)
+
+with st.form(key='my_form'):
+    slider_input = st.slider('My slider', 0, 10, 5, key='my_slider')
+    checkbox_input = st.checkbox('Yes or No', key='my_checkbox')
+    textbox_input = st.text_input("Your name", key="my_textbox")
+    submit_button = st.form_submit_button(label='Submit', on_click=form_callback)
+
+
+
+
+
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
        np.random.randn(20, 3),
