73e0c5f4e7b6593642d7754c422fd9be038bdcac
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2026-01-13 16:56:25 +0000

    U

--- home.py
+++ home.py
@@ -35,7 +35,7 @@ MyDB = "CS IA DB.db"
 conn = sql.connect(MyDB)
 
 cur = conn.cursor()
-cur.execute("ALTER TABLE task ADD COLUMN status TEXT")
+#cur.execute("ALTER TABLE task ADD COLUMN status TEXT")
 conn.commit() 
 
 conn.close()
