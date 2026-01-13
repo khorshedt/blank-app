fed5f13b71f63e2219ec247f3c9f22ac32194c63
Tarek Khorshed <Tarek_Khorshed@hotmail.com>
2026-01-13 18:13:21 +0000

    U

--- goal_planning.py
+++ goal_planning.py
@@ -21,7 +21,7 @@ st.markdown("## **My Tasks**")
 # Read and display Tasks
 df_task = pd.read_sql("SELECT * FROM Task", conn)
 task_titles = df_task['title'].tolist()
-
+st.write(df_task)
 
 st.markdown("## **My Goal Planning**")
 # Read data for Goal points from the database into a DataFrame and display it
